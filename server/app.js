092ea82db22d82d5f8cd1c18ec80861ef1f2d358
Scott Bromander <scott@primeacademy.io>
2017-04-05 10:00:07 -0500

    add build auto and server for static files

---
+++ server/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static('server/public'));
+
+app.get('/', function(req,res){
+    res.sendFile(path.resolve('server/public/views/index.html'));
+});
+
+app.listen(app.get('port'), function(){
+    console.log('Listening on port: ', app.get('port'));
+});
+
+module.exports = app;
